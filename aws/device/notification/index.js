888a49e7cb916d38ebd1cc833b1b309d3cddcd95
yukiho <is0436er@ed.ritsumei.ac.jp>
2018-11-15 11:52:14 +0900

    [update] LINEに通知が飛ぶようになった

--- aws/device/notification/index.js
+++ aws/device/notification/index.js
@@ -1,4 +1,6 @@
 const AWS = require("aws-sdk");
+const https = require('https');
+const querystring = require('querystring');
 
 AWS.config.update({
   region: "us-west-2",
@@ -6,6 +8,53 @@ AWS.config.update({
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
 
-}
+  const ChannelAccessToken = process.env['CHANNEL_ACCESS_TOKEN'];
+  const id = "U7b8ca61f99ffbc2e31a6e460614cc648";
+
+  const postData = JSON.stringify({
+    "messages": [{
+    "type": "text",
+    "text": "最適な温度になりました！"
+    }],
+    "to": id
+  });
+
+  const options = {
+    hostname: 'api.line.me',
+    path: '/v2/bot/message/push',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Content-Length': Buffer.byteLength(postData),
+      'Authorization': 'Bearer ' + ChannelAccessToken
+    },
+    method: 'POST',
+  };
+
+  //APIリクエスト
+  const req = https.request(options,  function(res){
+    res.setEncoding('utf8');
+    res.on('data', function (body) {
+      console.log(body);
+      context.succeed('handler complete');
+    });
+  }).on('error', function(e) {
+    context.done('error', e);
+    console.log(e);
+  });
+
+  req.on('error', function(e) {
+    const message = "通知に失敗しました. LINEから次のエラーが返りました: " + e.message;
+    console.error(message);
+    context.fail(message);
+  });
+
+  req.write(postData);
+  req.on('data', function (body) {
+    console.log(body);
+  });
+  req.end();
+
+  callback(null, 'Success!');
+};
