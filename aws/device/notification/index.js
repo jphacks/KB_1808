52496f2ebfd854b316b87a71dd4e1268d96cabdb
yukiho <is0436er@ed.ritsumei.ac.jp>
2018-11-17 01:41:24 +0900

    [update] nofitification完成

--- aws/device/notification/index.js
+++ aws/device/notification/index.js
@@ -10,15 +10,51 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context, callback) => {
 
+  const table = "shikuhack";
+
+  // クエリパラメータはGatewayで保証
+  const coasterMac = event['coaster_mac'];
+
+  const params = {
+    TableName: table,
+    Key: {
+      "coaster_mac": coasterMac
+    }
+  };
+
+  docClient.get(params, function(err, data) {
+    if (err) {
+      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+      callback(JSON.stringify(getErrorObj(context, "Internal Server Error.")));
+    } else {
+      console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+      if(data['Item'] === undefined){
+        callback(JSON.stringify(getErrorObj(context, "The coaster_mac doesn't register.")));
+        return;
+      }
+
+      const lineId = data['Item'].line_id;
+
+      pushMessage("最適な温度になりました", lineId);
+
+      callback(null, {
+        "statusCode": 200
+      });
+    }
+  });
+
+  callback(null, 'Success!');
+};
+
+function pushMessage(message, lineId){
   const ChannelAccessToken = process.env['CHANNEL_ACCESS_TOKEN'];
-  const id = process.env['ID'];
 
   const postData = JSON.stringify({
     "messages": [{
     "type": "text",
-    "text": "最適な温度になりました！"
+    "text": message
     }],
-    "to": id
+    "to": lineId
   });
 
   const options = {
@@ -37,17 +73,14 @@ exports.handler = async (event, context, callback) => {
     res.setEncoding('utf8');
     res.on('data', function (body) {
       console.log(body);
-      context.succeed('handler complete');
     });
   }).on('error', function(e) {
-    context.done('error', e);
     console.log(e);
   });
 
   req.on('error', function(e) {
     const message = "通知に失敗しました. LINEから次のエラーが返りました: " + e.message;
     console.error(message);
-    context.fail(message);
   });
 
   req.write(postData);
@@ -55,6 +88,12 @@ exports.handler = async (event, context, callback) => {
     console.log(body);
   });
   req.end();
+}
 
-  callback(null, 'Success!');
-};
+function getErrorObj(context, message){
+  return {
+    "status_code" : 400,
+    "request_id" : context.awsRequestId,
+    "message" : message
+  };
+}
